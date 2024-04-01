f7e5b257d58086982fcc3a99a273bfcec095095f
Elkasardelka <elzakshevskaya@gmail.com>
2024-04-01 16:15:40 +0300

    added new page Exhibitions

---
+++ app/components/Icons/Model.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Model = () => {
+  return (
+    <svg width="70px" height="70px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="#CCCCCC" strokeWidth="0.188"></g><g id="SVGRepo_iconCarrier"> <path d="M7 9.5L12 12M7 9.5V14.5L12 17M7 9.5L12 7L17 9.5M12 12L17 9.5M12 12V17M17 9.5V14.5L12 17M8 4H6C4.89543 4 4 4.89543 4 6V8M8 20H6C4.89543 20 4 19.1046 4 18V16M16 4H18C19.1046 4 20 4.89543 20 6V8M16 20H18C19.1046 20 20 19.1046 20 18V16" stroke="#0d6efd" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+  )
+}
+
+export default Model
