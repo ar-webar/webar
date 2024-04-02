9a5a5a47043398053cc44e9ca1f588c8b7d7ff90
Elkasardelka <elzakshevskaya@gmail.com>
2024-04-02 14:31:57 +0300

    added new page Monuments

--- .next/types/app/monuments/page.ts
+++ .next/types/app/monuments/page.ts
@@ -1,8 +1,8 @@
-// File: /Users/elburitto/venditio_3/webar/app/miroshnichenko/page.tsx
-import * as entry from '../../../../app/miroshnichenko/page.js'
+// File: /Users/elburitto/venditio_3/webar/app/monuments/page.tsx
+import * as entry from '../../../../app/monuments/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../../app/miroshnichenko/page.js')
+type TEntry = typeof import('../../../../app/monuments/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
